f064dad0a9aad5de7886aad5a6484f310b6c261b
malmilo6 <maxim.cernetchi@isa.utm.md>
2024-08-18 20:06:59 +0300

    init

---
+++ app/backend/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Table(models.Model):
+    qr_code = models.CharField(max_length=255, null=False)
+    status = models.BooleanField(default=False)
+    assigned_waited_id = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f"{self.id} - {self.status}"
+
+
+class Client(models.Model):
+    pass
+
+
+class Waiter(models.Model):
+    first_name = models.CharField(max_length=255, null=False)
+    last_name = models.CharField(max_length=255, null=False)
+    assigned_tables = models.ManyToManyField(Table, related_name='waiters')
+
+    def __str__(self):
+        return f"{self.first_name}, {self.last_name} - {self.assigned_tables}"
